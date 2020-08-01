080b703d01f9514cc0507c2f2ded3eca1e9c2c5d
Anuj kumar R <anujkumar.r@nutanix.com>
2020-08-01 13:20:34 +0530

    useState search example.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState();
+
+  const onSearchChangeHandler = (e) => {
+    // console.log(e.target.value);
+    setSearchQuery(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    window.open(`https://google.com/search?q=${searchQuery}`, "_blank");
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onSearchClick();
+      setSearchQuery("");
+    }
+  };
+
   return (
-    <div className="App">
-      <p>Welcome to react hooks</p>
+    <div className="form">
+      <h1>Welcome!!</h1>
+      <input
+        type="text"
+        value={searchQuery}
+        onKeyPress={handleKeyPress}
+        onChange={onSearchChangeHandler}
+      />
+      <button onClick={onSearchClick}>Search</button>
     </div>
   );
 }
